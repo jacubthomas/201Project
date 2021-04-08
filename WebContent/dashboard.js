b1b14f042c4065a6239cb1d581fbaa1a3e2dd7e4
Justin Lee <jlee7605@usc.edu>
2021-04-07 19:31:57 -0700

    login and register

--- WebContent/dashboard.js
+++ WebContent/dashboard.js
@@ -1,4 +1,4 @@
 //logging to console all local storage items
 for (var i = 0; i < localStorage.length; i++){
-    console.log(localStorage.getItem(localStorage.key(i)))
+    console.log(localStorage.key(i) + ': ' +localStorage.getItem(localStorage.key(i)))
 }
