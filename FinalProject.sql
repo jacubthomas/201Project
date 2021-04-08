d243c58149bc1923fe63c922f305b6b46c337db8
Timothy Wu <wutimoth@usc.edu>
2021-04-07 17:08:38 -0700

    Changed mapping of security table to the user
    User now has no foreign keys, security table has foreign key to user

--- FinalProject.sql
+++ FinalProject.sql
@@ -3,26 +3,24 @@ CREATE DATABASE FinalProject;
 
 Use FinalProject;
 
-CREATE TABLE Security (
-	SecurityID INT Primary KEY AUTO_INCREMENT,
-    SecurityQuestion VARCHAR(100) NOT NULL,
-    CustomAnswer VARCHAR(100) NOT NULL
-);
-
 CREATE TABLE Users (
 	UserID INT PRIMARY KEY AUTO_INCREMENT,
-    PhotoID INT NOT NULL,
     username VARCHAR(15) NOT NULL,
     password_ VARCHAR(15) NOT NULL,
     firstname VARCHAR(15) NOT NULL,
     lastname VARCHAR(15) NOT NULL,
     joined_on VARCHAR(15) NOT NULL,
     date_of_birth VARCHAR(15) NOT NULL,
-    SecurityID INT NOT NULL,
-    FOREIGN KEY fk1(SecurityID) references Security(SecurityID),
     public_status INT NOT NULL #1 = public, 2 = private
 );
 
+CREATE TABLE Security (
+	SecurityID INT Primary KEY AUTO_INCREMENT,
+    UserID INT NOT NULL,
+    FOREIGN KEY fk1(UserID) references Users(UserID),
+    SecurityQuestion VARCHAR(100) NOT NULL,
+    CustomAnswer VARCHAR(100) NOT NULL
+);
 CREATE TABLE Friends (
 	FriendsID INT PRIMARY KEY AUTO_INCREMENT,
     UserID1 INT NOT NULL,
