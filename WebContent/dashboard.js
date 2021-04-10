851eccd5fbb73aafd94a5622f8e7672586a53ee6
Jacob Harrington <jharring@usc.edu>
2021-04-09 18:47:36 -0700

    built upon post functionality with private mode which allows for a recipient entry as well as changes the color scheme.

--- WebContent/dashboard.js
+++ WebContent/dashboard.js
@@ -28,3 +28,18 @@ function post(){
 	.then(response => response.text())
 	.then(response => console.log(response))
 }
+
+function privacyToggle(){
+	var toggle = document.getElementById("switch1");
+	var masterdiv = document.getElementById("say_more");
+	var txtarea = document.getElementById("say_more_txt");
+	var probst = document.getElementById("login");
+	var friend = document.getElementById("pickafriend");
+	var label = document.getElementById("picklabel");
+	toggle.classList.toggle("nightmode");
+	masterdiv.classList.toggle("nightmode");
+	txtarea.classList.toggle("nightmode2");
+	probst.classList.toggle("nightmode2");
+	friend.classList.toggle("hidden");
+	label.classList.toggle("hidden");
+}
